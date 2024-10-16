4bf0e498e0f30a9232432213a8a18db361f69742
Phuc Chau <phucnchau@gmail.com>
2024-10-16 19:31:36 -0400

    Add fetch api logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    fetchAllCoinData().catch(console.error);
+  }, []);
+
+  const fetchAllCoinData = async () => {
+    let query = `https://min-api.cryptocompare.com/data/all/coinlist?api_key=${API_KEY}`;
+
+    const response = await fetch(query);
+    const json = await response.json();
+    setList(json);
+  };
 
   return (
-    <div></div>
-  )
-}
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <ul>
+        {list && Object.entries(list.Data).map(([coin]) => 
+          list.Data[coin].PlatformType === "blockchain" ? (
+            <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+          ) : null
+        )}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
